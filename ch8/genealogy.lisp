3070ac6d67d4e551910a727db842579e78203830
elias <elias.jm.garcia@gmail.com>
2017-07-26 17:09:37 -0500

    finished subproblem b) of chapter 8's exercise, started c)

--- ch8/genealogy.lisp
+++ ch8/genealogy.lisp
@@ -16,6 +16,10 @@
 ;;LOAD DATABASE 'FAMILY'
 (load "~/projects/learning/cl_gitsc_solutions/ch8/family-database.lisp")
 
+;; problem A) description:
+;; Create functions FATHER, MOTHER, PARENTS, and CHILDREN that return the respective names 
+;; of the individuals by given person's relation. Return nil if none exist or nil is provided.
+
 (defun FATHER (person)
    "returns the father of a given person from the family dbs"
    (if (null person)
@@ -42,3 +46,26 @@
                       (cond ((equal person (father (car fam))) (car fam))
                          ((equal person (mother (car fam))) (car fam))))
             family))))
+
+;; problem B) description:
+;; Create SIBLINGS fn that returns list of a person's siblings, including *genetic half-siblings*
+
+(defun SIBLINGS (person)
+   "finds the biological siblings, including step, of a given person from the family dbs"
+   (cond ((null person) nil)
+      ((and
+          (null (father person))
+          (null (mother person)))
+         nil)
+      ((null (father person))
+         (children (mother person)))
+      ((null (mother person))
+         (children (father person)))
+      (t (set-difference (union (children (father person))
+                            (children (father person))) (list person)))))
+
+;; problem C) description:
+;; Create MAPUNION applicative operator that takes a fn and a list, applies the fn to each elem in
+;; the list, then applies union to the results of the previous operation
+
+(defun MAPUNION (fn ls))
